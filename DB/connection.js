d5a6991449db0cd7d0d7c218b0a954daf9171598
ArwaAbdelal <arwaabdelall1@gmail.com>
2025-02-13 20:17:46 +0200

    database connection

--- DB/connection.js
+++ DB/connection.js
@@ -1,7 +1,8 @@
 import Sequelize from 'sequelize';
 
-export const sequelize = new Sequelize('trips', 'root', 'root', {
-  host: 'localhost',
+export const sequelize = new Sequelize('freedb_TripProject', 'freedb_arwaFirst', '2xRAWf5uc%@6z2n', {
+  host: 'sql.freedb.tech',
+  port:3306,
   dialect:'mysql' /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
 });
 
