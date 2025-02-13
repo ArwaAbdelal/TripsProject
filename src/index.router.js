814da64941546c6c4155f6d884cf35c9ef74c6e5
ArwaAbdelal <arwaabdelall1@gmail.com>
2025-02-13 21:01:57 +0200

    url edit

--- src/index.router.js
+++ src/index.router.js
@@ -2,6 +2,7 @@ import { connectDB } from "../DB/connection.js";
 import authRouter from './modules/auth/auth.router.js';
 import touristRouter from './modules/tourist/tourist.router.js';
 import tripRouter from './modules/trip/trip.router.js';
+import { AppError } from "./utils/AppError.js";
 import cors from 'cors';
 
 const initApp=(app,express)=>{
@@ -11,7 +12,9 @@ app.use(cors());
 app.use('/auth',authRouter);
 app.use('/tourist',touristRouter);
 app.use('/trip',tripRouter);
-
+app.get('*',(req,res,next)=>{
+    return next (new AppError("page not found",404));
+});
 
 };
 
