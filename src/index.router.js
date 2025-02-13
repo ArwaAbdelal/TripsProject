2698fd8ebecaabeb1f6c3e728b23cd92868b3015
ArwaAbdelal <arwaabdelall1@gmail.com>
2025-02-13 19:54:09 +0200

    edit

--- src/index.router.js
+++ src/index.router.js
@@ -2,11 +2,12 @@ import { connectDB } from "../DB/connection.js";
 import authRouter from './modules/auth/auth.router.js';
 import touristRouter from './modules/tourist/tourist.router.js';
 import tripRouter from './modules/trip/trip.router.js';
+import cors from 'cors';
 
 const initApp=(app,express)=>{
 connectDB();
 app.use(express.json());
-
+app.use(cors());
 app.use('/auth',authRouter);
 app.use('/tourist',touristRouter);
 app.use('/trip',tripRouter);
